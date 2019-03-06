af106d07e8f7784e6a24506cfe205f187206dccb
JunXingLiu <jliu73@nait.ca>
2019-03-05 17:21:19 -0700

    Finished expandable list

--- app/src/main/java/ca/nait/jliu73/tasklist/MainActivity.java
+++ app/src/main/java/ca/nait/jliu73/tasklist/MainActivity.java
@@ -2,18 +2,53 @@ package ca.nait.jliu73.tasklist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.nait.jliu73.tasklist.adapter.TaskHeaderAdapater;
+import ca.nait.jliu73.tasklist.models.TaskDetails;
+import ca.nait.jliu73.tasklist.models.TaskHeader;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
+    private RecyclerView rv;
+    private TaskHeaderAdapater adapater;
+    private List<TaskHeader>  headerList;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = (RecyclerView)findViewById(R.id.rv);
+        getHeaderList();
+        adapater = new TaskHeaderAdapater(headerList);
+        rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        rv.setAdapter(adapater);
     }
 
     @Override
     public void onClick(View v) {
+
+    }
+
+    public void getHeaderList()
+    {
+        headerList = new ArrayList<>(6);
+        for(int i = 0; i < 6; i++)
+        {
+            List<TaskDetails> details = new ArrayList<>(3);
+            details.add(new TaskDetails("LOL"));
+            details.add(new TaskDetails("LOL1"));
+            details.add(new TaskDetails("LOL1111"));
+            headerList.add(new TaskHeader("this is " + i, details));
+        }
 
     }
 }
